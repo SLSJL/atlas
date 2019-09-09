b81800964758451c25cce3c3bc7a7d07db2b44cb
mrraindrop <350916525@qq.com>
2019-09-09 13:03:41 +0800

    added promoting video

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -4,10 +4,11 @@
       <Navigation class="home-navigation"></Navigation>
       <Section1 class="home-sections-1"></Section1>
     </section>
-    
+
     <Section2 class="home-sections-2"></Section2>
     <Section3></Section3>
     <section4></section4>
+    <Section51></Section51>
     <Section5></Section5>
     <Section6></Section6>
     <Wooter></Wooter>
@@ -19,6 +20,7 @@ import Section1 from "./section1";
 import Section2 from "./section2";
 import Section3 from "./section3";
 import Section4 from "./section4";
+import Section51 from "./section5.1.vue";
 import Section5 from "./section5";
 import Section6 from "./section6";
 import Wooter from "./../../components/wooter/";
@@ -30,6 +32,7 @@ export default {
     Section2,
     Section3,
     Section4,
+    Section51,
     Section5,
     Section6,
     Wooter
---
+++ src/views/home/section5.1.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="contianer-fluid senction-51">
+    <video
+      class="video-top"
+      src="http://158.132.186.221:9001/file/video/promotion-video.mp4"
+      controls
+    >Your browser does not support the video.</video>
+  </div>
+</template>
+<script>
+export default {
+  components: {},
+  mounted() {}
+};
+</script>
+
+<style lang="less" scoped>
+.senction-51 {
+  position: relative;
+  width: 100%;
+  height: 600px;
+  background-color: #eee;
+  border-bottom: 2px solid #314a7f;
+  background-image: url("./../../assets/images/home/video-bg.jpg");
+  background-size: 130% auto;
+  overflow: hidden;
+
+  .video-top {
+    height: 100%;
+  }
+}
+
+@media only screen and (min-width: 48em) and (max-width: 75em) {
+  .senction-51 {
+    width: 100%;
+    height: auto;
+    .video-top {
+      width: 100%;
+      height: auto;
+    }
+  }
+}
+
+@media only screen and (max-width: 48em) {
+  .senction-51 {
+    width: 100%;
+    height: auto;
+    width: 100%;
+    .video-top {
+      width: 100%;
+      height: auto;
+    }
+  }
+}
+</style>
